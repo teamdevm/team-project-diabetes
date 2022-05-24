7727ef709e639df20b2282e66dddd488b0e41b8e
Dmitry <chiruhind@gmail.com>
2022-05-24 17:28:18 +0500

    Добавил структуру проектов

---
+++ Diabetes.Persistence/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Diabetes.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DefaultConnection"];
+            //services.AddDbContext<>(opt=>opt.UseSqlite(connectionString));
+            return services;
+        }
+    }
+}
