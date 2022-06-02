e8a42e22436bf356c19d3e2f63c88840a4f869c1
Dmitry <chiruhind@gmail.com>
2022-06-02 20:03:24 +0500

    Add cookie lifetime

--- Diabetes.MVC/Startup.cs
+++ Diabetes.MVC/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Diabetes.Application;
 using Diabetes.Persistence;
 using Diabetes.Persistence.Contexts;
@@ -25,6 +26,10 @@ namespace Diabetes.MVC
             services.AddControllersWithViews();
             services.AddApplication();
             services.AddPersistence(Configuration);
+            services.Configure<SecurityStampValidatorOptions>(options =>
+            {
+                options.ValidationInterval = TimeSpan.FromMinutes(10);
+            });
             services.AddIdentity<Account, IdentityRole>(opts=> {
                     opts.User.RequireUniqueEmail = true;
                     opts.Password.RequiredLength = 6;
