668bff722b4e342e6e15ccf04ae8931455d1b6a4
archomai <archomai@yandex.ru>
2022-06-02 22:40:42 +0500

    Lock for other pages (besides adding glucose) added

--- Diabetes.MVC/Controllers/MealController.cs
+++ Diabetes.MVC/Controllers/MealController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Diabetes.MVC.Controllers
 {
     public class MealController:Controller
     {
+        [Authorize]
         public IActionResult AddMeal()
         {
             return View();
