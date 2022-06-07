71d638ff318b61833f1e13a03d1e602d68686215
Dmitry <chiruhind@gmail.com>
2022-06-07 23:15:14 +0500

    Add show more button on home page

--- Diabetes.Application/ActionHistoryItem/Commands/GetAllActionHistoryItems/GetAllActionHistoryItemsHandler.cs
+++ Diabetes.Application/ActionHistoryItem/Commands/GetAllActionHistoryItems/GetAllActionHistoryItemsHandler.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Diabetes.Application.ActionHistoryItems.Commands.GetActionHistoryItems;
 using Diabetes.Application.Interfaces;
 using Diabetes.Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace Diabetes.Application.ActionHistoryItems.Commands.GetActionHistoryItems
+namespace Diabetes.Application.ActionHistoryItem.Commands.GetAllActionHistoryItems
 {
-    public class GetActionHistoryItemsCommandHandler : IRequestHandler<GetActionHistoryItemsCommand, List<Domain.ActionHistoryItem>>
+    public class GetAllActionHistoryItems: IRequestHandler<GetAllActionHistoryItemsCommand, List<Domain.ActionHistoryItem>>
     {
         private readonly IGlucoseLevelDbContext _dbContextClucose;
         private readonly INoteInsulinDbContext _dbContextInsulin;
 
-        public GetActionHistoryItemsCommandHandler(
+        public GetAllActionHistoryItems(
             IGlucoseLevelDbContext dbContextClucose, INoteInsulinDbContext dbContextInsulin)
         {
             _dbContextClucose = dbContextClucose;
             _dbContextInsulin = dbContextInsulin;
         }
 
-        public async Task<List<Domain.ActionHistoryItem>> Handle(GetActionHistoryItemsCommand request, CancellationToken cancellationToken)
+        public async Task<List<Domain.ActionHistoryItem>> Handle(GetAllActionHistoryItemsCommand request, CancellationToken cancellationToken)
         {
             var queryInsulin = await _dbContextInsulin.InsulinNotes
                 .Where(p => p.UserId == request.UserId)
                 .OrderByDescending(p => p.CreationDateTime)
-                .Take(request.Number)
                 .Select(ins=> new Domain.ActionHistoryItem
                 {
                     Id = ins.Id,
@@ -42,7 +42,6 @@ namespace Diabetes.Application.ActionHistoryItems.Commands.GetActionHistoryItems
             var queryGlucose = await _dbContextClucose.GlucoseLevels
                 .Where(p => p.UserId == request.UserId)
                 .OrderByDescending(p => p.CreationDateTime)
-                .Take(request.Number)
                 .Select(glu=> new Domain.ActionHistoryItem
                 {
                     Id = glu.Id,
@@ -57,7 +56,6 @@ namespace Diabetes.Application.ActionHistoryItems.Commands.GetActionHistoryItems
 
             var actions = queryInsulin.Union(queryGlucose)
                 .OrderByDescending(a => a.CreationDateTime)
-                .Take(request.Number)
                 .ToList();
 
             return actions;
