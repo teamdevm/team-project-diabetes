13d8f3a6a9e32c870ef21aca792605acdbdadcd5
archomai <archomai@yandex.ru>
2022-06-21 00:08:13 +0500

    Interface version 1.0

---
+++ Diabetes.MVC/Controllers/StatisticsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Diabetes.MVC.Models;
+using System.Threading.Tasks;
+
+namespace Diabetes.MVC.Controllers
+{
+    public class StatisticsController: Controller
+    {
+        private readonly IMediator _mediator;
+
+        public StatisticsController(IMediator mediator) => _mediator = mediator;
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
